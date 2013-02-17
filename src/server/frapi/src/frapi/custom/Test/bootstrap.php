2d402d84765eba80ffc14b317a86f4e6420d5ca5
KOMATSU Issei <issei@mizuhataya.com>
2013-02-17 15:19:14 +0900

    phpunitのサンプルを追加

---
+++ src/server/frapi/src/frapi/custom/Test/bootstrap.php
@@ -0,0 +1,5 @@
+<?php
+set_include_path(get_include_path() . ':' . dirname(__file__) . '/unit');
+
+require_once('Cheekit_PHPUnit_Framework_TestCase.class.php');
+?>
--- src/server/frapi/src/frapi/custom/Test/unit/Cheekit_PHPUnit_Framework_TestCase.class.php
+++ src/server/frapi/src/frapi/custom/Test/unit/Cheekit_PHPUnit_Framework_TestCase.class.php
@@ -1,8 +1,4 @@
 <?php
 class Cheekit_PHPUnit_Framework_TestCase extends PHPUnit_Framework_TestCase
 {
-    public function testOk()
-    {
-        $this->assertEquals('cheekit', 'cheekit');
-    }
 }
