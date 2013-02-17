112476e5db199b72a22d529602854a9fab2e88eb
KOMATSU Issei <issei@mizuhataya.com>
2013-02-18 00:47:30 +0900

    SpotMaster から User_id を削除

--- src/server/sql/init/create_SpotMaster.sql
+++ src/server/sql/init/create_SpotMaster.sql
@@ -3,7 +3,6 @@ CREATE TABLE SpotMaster
     id          int(11) NOT NULL AUTO_INCREMENT,
     name        text    NOT NULL,
     number      int(11) NOT NULL,
-    user_id     int(11) NOT NULL,
     lat         decimal(9, 6) NOT NULL,
     lon         decimal(9, 6) NOT NULL,
     description text,
