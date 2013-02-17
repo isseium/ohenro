2c33ee23e6c261e19a2ca89d04dbe993b1373d6d
KOMATSU Issei <issei@mizuhataya.com>
2013-02-17 17:33:13 +0900

    jenkins向け設定

--- src/server/frapi/src/frapi/custom/Test/bootstrap.php
+++ src/server/frapi/src/frapi/custom/Test/bootstrap.php
@@ -1,5 +1,17 @@
 <?php
 set_include_path(get_include_path() . ':' . dirname(__file__) . '/unit');
+set_include_path(get_include_path() . ':' . dirname(__file__) . '/../Model/');
+
 
 require_once('Cheekit_PHPUnit_Framework_TestCase.class.php');
+
+/**
+ * オートロード設定
+ */
+spl_autoload_register(function ($class_name){
+    include $class_name . ".class.php";
+});
+
+
+
 ?>
