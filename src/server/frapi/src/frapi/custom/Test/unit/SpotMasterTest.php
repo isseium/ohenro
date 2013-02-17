e0e46d9afd99f9a466e22bbefb56c4ba01d36d37
KOMATSU Issei <issei@mizuhataya.com>
2013-02-17 17:55:36 +0900

    jenkins向けhook作動チェック用コミット

--- src/server/frapi/src/frapi/custom/Test/unit/SpotMasterTest.php
+++ src/server/frapi/src/frapi/custom/Test/unit/SpotMasterTest.php
@@ -5,4 +5,9 @@ class SpotMasterTest extends Cheekit_PHPUnit_Framework_TestCase
         $spotMaster = new SpotMaster();
         $this->assertNotNull($spotMaster);
     }
+
+    public function test_全体取得(){
+        $spotMaster = new SpotMaster();
+        $this->assertEquals($spotMaster->getAllSpots(), array("id" => 1, "name" => "ほげ"));
+    }
 }
