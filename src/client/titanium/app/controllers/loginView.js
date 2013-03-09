1db9fc1483040045b7f35ad9eaf7f09b8650d107
itochin <investi0173gate@yahoo.co.jp>
2013-03-10 00:13:43 +0900

    ユーザ登録API実装

--- src/client/titanium/app/controllers/loginView.js
+++ src/client/titanium/app/controllers/loginView.js
@@ -1,9 +1,76 @@
-$.title = "タイトル";
+var ApiMapper = require("apiMapper").ApiMapper;
+var apiMapper = new ApiMapper();
+var fbSocial_type = 0;
+var TwSocial_type = 1;
+
+
 Ti.Facebook.appid = 606983742650457;
 Ti.Facebook.permissions = ['publish_stream','read_stream'];
 
-// Ti.Facebook.logout();
-// Ti.Facebook.authorize();
+$.facebookButton.addEventListener('click', function() {
+	if (Ti.Facebook.loggedIn){
+	    var accTo = Titanium.Facebook.getAccessToken();
+	    // alert(accTo);
+	    var name = viewFbInfo(accTo);
+	    var social_secret = ''
+	    //ユーザ登録
+		userRegister(name,fbSocial_type,accTo,social_secret);
+	}else{
+	 	Titanium.Facebook.authorize();	
+	}
+});
+
+var viewFbInfo = function(accTo){
+    Ti.Facebook.requestWithGraphPath(
+	    'me?fields=name,picture',
+	    {access_token:accTo},
+	    "GET",
+	    function(e) {
+	        if (e.success) {
+	            var obj = JSON.parse(e.result);
+	            // alert(obj.picture.data.url);
+	            // alert("Success: " + e.result);
+	            // $.image = "http://graph.facebook.com/"+obj.uid+"/picture";
+				var bfImage = Ti.UI.createImageView({
+					image:obj.picture.data.url,
+					width:100,
+					height:100,
+					top:200
+				});
+				$.loginView.add(bfImage);
+				
+				var fbName = Ti.UI.createLabel({
+					top:280,
+					text:obj.name
+				});
+				$.loginView.add(fbName);
+				$.facebookButton.hide();
+				return obj.name;
+	        }
+	    }
+	);	
+};
+
+/**
+ * ユーザ登録
+ */
+var userRegister = function(name,social_type,social_token,social_secret){
+	apiMapper.userregisterApi(
+		name,
+		social_type,
+		social_token,
+		social_secret,
+		function(){
+			// 成功したとき
+			var json = eval('(' + this.responseText + ')');
+			Alloy.Globals.user = json.user;		
+		},
+		function(e){
+			// 失敗したとき
+			alert(e.result + 'データの取得に失敗しました。');
+		}
+	);	
+};
 
 $.twitterButton.addEventListener('click', function(e) {
 	Ti.include('twitter_api.js');
@@ -14,43 +81,3 @@ $.twitterButton.addEventListener('click', function(e) {
 	});
 	twitterApi.init(); 
 });
-
-// $.facebookInfo.addEventListener('click', function(e) {
-    // // Ti.Facebook.requestWithGraphPath(
-        // // 'me',
-        // // {},
-        // // "GET",
-        // // function(e) {
-            // // if (e.success) {
-                // // var obj = JSON.parse(e.result);
-                // // // alert("Success: " + obj.name);
-				// // $.image = Ti.UI.createImageView({
-					// // image:obj.pic_square,
-					// // left:10,
-					// // width:50,
-					// // height:50
-				// // });
-            // // }
-        // // }
-    // // );
-	// var query = "SELECT uid, name, pic FROM user ";
-	// query +=  "where uid  = " + Titanium.Facebook.uid;
-	// Ti.API.info('user id ' + Titanium.Facebook.uid);
-	// Titanium.Facebook.request('fql.query', {query: query},  function(r) {
-		// if (!r.success) {
-			// if (r.error) {
-				// alert(r.error);
-			// } else {
-				// alert("call was unsuccessful");
-			// }
-			// return;
-		// }
-		// var result = JSON.parse(r.result);
-		// $.image = Ti.UI.createImageView({
-			// image:r.pic,
-			// left:10,
-			// width:50,
-			// height:50
-		// });
-	// });
-// });
