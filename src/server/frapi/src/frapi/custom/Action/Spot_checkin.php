9463f9f7ead9d978bbcaa9c1af8916fefb51a3b5
KOMATSU Issei <issei@mizuhataya.com>
2013-03-20 17:45:17 +0900

    シェアコメントを本番用に変更

--- src/server/frapi/src/frapi/custom/Action/Spot_checkin.php
+++ src/server/frapi/src/frapi/custom/Action/Spot_checkin.php
@@ -103,7 +103,8 @@ class Action_Spot_checkin extends Frapi_Action implements Frapi_Action_Interface
         $checkin = CheckinManager::generateByCheckinId($spot->checkin_id);
 
         // シェア設定
-        $message = "これはテストだよー time=" . $time;
+        // $message = "これはテストだよー time=" . $time;
+        $message = sprintf("%s (%s を巡礼しました) #ぼくの細道", mb_strimwidth($comment, 0, 80, '...'), $spot->name);
         ShareQueue::enqueue($user, $checkin->id, $message);
 
         // レスポンス
