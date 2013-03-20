1e5f0e6ffad11d59a2faf0a9515d41807c28ccec
KOMATSU Issei <issei@mizuhataya.com>
2013-03-20 17:26:08 +0900

    フォーマット変更

--- src/client/titanium/app/lib/app_config.sample.js
+++ src/client/titanium/app/lib/app_config.sample.js
@@ -1,8 +1,16 @@
 /**
  * アプリケーション設定
+ *
+ * @author isseium
+ *
+ * 環境別に変更する必要がある項目をまとめています。
+ *
+ * 各種項目を埋めて app_config.js にファイル名を変更して利用してください。
+ * なお、app_config.js は重要な機密データなので、コミットしないようにしてください（.gitignore に除外設定を記載しています）
  */
-Alloy.Globals.app = new Object();
-Alloy.Globals.app.twitter_consumer_token = "";
-Alloy.Globals.app.twitter_consumer_secret = "";
-Alloy.Globals.app.facebook_appid = "";
-Alloy.Globals.app.api_endpoint = "";
+Alloy.Globals.app = {
+    twitter_consumer_token: "",
+    twitter_consumer_secret: "",
+    facebook_appid: "",
+    api_endpoint: "",
+};
