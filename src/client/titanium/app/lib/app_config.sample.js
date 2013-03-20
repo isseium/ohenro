aa5d70143b88c49229c4ebdd3381b3d8eae6e463
KOMATSU Issei <issei@mizuhataya.com>
2013-03-20 15:55:05 +0900

    アプリケーション設定用ライブラリ追加

---
+++ src/client/titanium/app/lib/app_config.sample.js
@@ -0,0 +1,9 @@
+/**
+ * アプリケーション設定
+ */
+Alloy.Globals.app = new Object();
+Alloy.Globals.app.twitter_consumer_token = "";
+Alloy.Globals.app.twitter_consumer_secret = "";
+Alloy.Globals.app.facebook_consumer_token = "";
+Alloy.Globals.app.facebook_consumer_secret = "";
+Alloy.Globals.app.api_endpoint = "";
